7bb5ecd09c6c699c1b81f131bc46cd4986bd4e02
Fabiano <fasnaweb2020@gmail.com>
2022-03-10 20:31:00 -0300

    Primeiros passos com git comandos basicos

--- $02_GITBASICO/$010_CriandoContaGithub.java
+++ $02_GITBASICO/$010_CriandoContaGithub.java
@@ -1,5 +1,8 @@
 package $02_GITBASICO;
 
 public class $010_CriandoContaGithub {
-
+	/*
+	 * Primeiro voce entra no site do github e cria uma conta,que pode ser uma conta gratuita ou paga
+	 * Se voce estiver trabalhando em uma empresa  e logico que voce vai ciar uma conta privada. 
+	 */
 }
---
+++ $02_GITBASICO/$011_ComandosBasicos.java
@@ -0,0 +1,48 @@
+package $02_GITBASICO;
+
+public class $011_ComandosBasicos {
+	/*
+	 * COMANDO GIT INIT
+	 * 	Primeiro é necessario criar uma pasta e depois inicializa-la para ser seu repositorio
+	 *  git init -> Inicia seu repositorio.
+	 *  
+	 * ---------------------------------------------------------------------------
+	 * 
+	 * COMANDO GIT ADD .
+	 *  E o comando que adiciona as alteracoes feitas nos seu arquivos,ficando essas alteracoes 
+	 *  prontas para serem comitadas.Toda vez que voce altera aumenta seus arquivos,o git pede que voce 
+	 *  salve essas alteracoes e que depois voce transcreva uma breve descricao sobre essas alteracoes.
+	 *  
+	 *  git add + o nome do arquivo ou -> Aqui voce adiciona o arquivo em si
+	 *  git add . -> Aqui voce adiciona todos arquivos que foram alterados.
+	 * ---------------------------------------------------------------------------
+	 * 
+	 * DESVINCULANDO UM ARQUIVO -> RM
+	 *  Assim como voce pode adicionar alteracoes de um arquivo,voce também pode remover alteracoes de um arquivo
+	 *  e tambem pode remover esse arquivo de forma permanente.
+	 *  
+	 *  Assim que voce adiciona algo se voce quizer remover use
+	 *  git rm + o nome do arquivo
+	 *  
+	 *  Agora se voce quizer realmente remover esse arquivo use
+	 *  git rm -f + o nome do arquivo
+	 *  
+	 * -----------------------------------------------------------------------------
+	 * 
+	 * GIT COMMIT E UMA DESCRICAO DESSA PORCAO DE TRABALHO
+	 *  git commit -m +(Descricao do commit)
+	 *  
+	 *  O commit e feito depois que voce adiciona as alteracoes dos seus arquivos
+	 *  
+	 * ------------------------------------------------------------------------------
+	 * 
+	 * GIT STATUS - SITUACAO DO PROJETO
+	 *  git status devolve a situacao no momento em que esta seu proeto,ou seja se tem dados alterados ou se foi inseridos
+	 *  novos ou se algo foi deletado ou se tem arquivos alterados e vc ainda nao adicionou, ou se falta fazer um commit.
+	 *  Enfim tras a situacao.
+	 *  
+	 *  git status
+	 *  
+	 * ------------------------------------------------------------------------------
+	 */
+}
