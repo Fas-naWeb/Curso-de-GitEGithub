7655ee391c71622eb7621f2bb90294e015db1856
Fabiano <fasnaweb2020@gmail.com>
2022-03-12 23:08:26 -0300

    Bora entender git log

--- $04_GITHUB_AVANCADO/$016_gitPullBaixandoCommits.java
+++ $04_GITHUB_AVANCADO/$016_gitPullBaixandoCommits.java
@@ -3,6 +3,8 @@ package $04_GITHUB_AVANCADO;
 public class $016_gitPullBaixandoCommits {
 	/*
 	 *  GIT PULL
-	 *    O git pull ele atualiza seus commits local com os commits do servidor.
+	 *    O git pull ele atualiza seus commits local com os commits do servidor.Na verdade o git pull e um comando
+	 *    que dois em um,porque ele verifica e ja atualiza.
+	 *    Então primeiro ele faz um fetch e depois atualiza.
 	 */
 }
---
+++ $04_GITHUB_AVANCADO/$018_GitLogVerificandoHistorico.java
@@ -0,0 +1,9 @@
+package $04_GITHUB_AVANCADO;
+
+public class $018_GitLogVerificandoHistorico {
+	/*
+	 *  DIT LOG
+	 *   E uma funcao do git que te ajuda a verificar o que ja foi feito,ele te da um historico dos seus commits.
+	 *   
+	 */
+}
