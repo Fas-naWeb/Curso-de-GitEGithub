1641d111e7ef30d24ff951d0f3c8812e6aa927d0
Fabiano <fasnaweb2020@gmail.com>
2022-03-12 21:19:41 -0300

    Entendendo git fetch

--- $04_GITHUB_AVANCADO/$016_gitPullBaixandoCommits.java
+++ $04_GITHUB_AVANCADO/$016_gitPullBaixandoCommits.java
@@ -1,5 +1,8 @@
 package $04_GITHUB_AVANCADO;
 
 public class $016_gitPullBaixandoCommits {
-
+	/*
+	 *  GIT PULL
+	 *    O git pull ele atualiza seus commits local com os commits do servidor.
+	 */
 }
---
+++ $04_GITHUB_AVANCADO/$017_GitFetchAtualizandoSuasBranchs.java
@@ -0,0 +1,15 @@
+package $04_GITHUB_AVANCADO;
+
+public class $017_GitFetchAtualizandoSuasBranchs {
+	/*
+	 *  GIT FETCH 
+	 *    O git fetch e um comando que te mostra se sua branch esta atualizada ou não,ele faz um comparativo 
+	 *    entre seu repositorio local com o repositorio do github,apos ele voce da um git status que mostra a 
+	 *    situacao da branch. O fetch, ele nao atualiza ele simplesmente tras a informacao de como esta  a relacao 
+	 *    entre os dois repositorios.
+	 *    
+	 *    Resumindo 
+	 *    git fetch
+	 *    git status
+	 */
+}
