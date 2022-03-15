d70d39e9e25ca39d88f65d65f0bef528895893f3
Fabiano <fasnaweb2020@gmail.com>
2022-03-14 21:36:47 -0300

    Entendendo o comando git merge

---
+++ $04_GITHUB_AVANCADO/$022_GitMerge.java
@@ -0,0 +1,15 @@
+package $04_GITHUB_AVANCADO;
+
+public class $022_GitMerge {
+	/*
+	 * 	GIT MERGE
+	 * 	  Esse comando mescla as branchs a branch principal.
+	 *    git merge master
+	 *    
+	 *    Sempre que voce quizer mergea uma branch a branch principal,primeiro voce entra da branch principal 
+	 *    que e a master depois voce faz o merge.
+	 *    
+	 *    git checkout master
+	 *    git merge login
+	 */
+}
