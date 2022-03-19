67effe1daa80943037adce60edcfd6edeff4d541
Fabiano <fasnaweb2020@gmail.com>
2022-03-19 23:16:30 +0000

    Entedendo git rebase

--- $04_GITHUB_AVANCADO/$023_GitRebase.java
+++ $04_GITHUB_AVANCADO/$023_GitRebase.java
@@ -1,7 +1,18 @@
 package $04_GITHUB_AVANCADO;
 
 public class $023_GitRebase {
-	/*
-	 * 	
-	 */
+  /*
+   *  GIT REBASE
+   *    Quando dois desenvlvedores trabalham separados,cada um fazendo uma parte do codigo,acontece de 
+   *    um subir seu codigo na frente do outro,e isso e normal.Porem quando o segundo desenvolvedor for
+   *    subir seu codigo vai haver divergencias,o chamado conflito,onde a master ja nao vai esta como 
+   *    estava quando os dois criaram sua ramificacao.
+   *    
+   *       Ha duas maneiras de resolver isso:
+   *         1° - Voce pode fazer um merge da master em sua branch, no caso a que voce esta trabalhando
+   *              Porem os commits feito por voce e pelo outro programador fica todos baguncado.
+   *         
+   *         2° - Fazendo o git rebase,que vai jogar seu codigo em cima do ultimo merge feito na master 
+   *              entao seu commits vai ficar separado do programador anterior,vai fica no topo dos logs. 
+   */
 }
