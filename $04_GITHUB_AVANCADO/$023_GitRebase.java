5838cd0f35941389aee59eecb3865eb4421b9a88
Fabiano <fasnaweb2020@gmail.com>
2022-03-24 21:26:34 -0300

    Recapitulando git rebase

--- $04_GITHUB_AVANCADO/$023_GitRebase.java
+++ $04_GITHUB_AVANCADO/$023_GitRebase.java
@@ -8,11 +8,11 @@ public class $023_GitRebase {
    *    subir seu codigo vai haver divergencias,o chamado conflito,onde a master ja nao vai esta como 
    *    estava quando os dois criaram sua ramificacao.
    *    
-   *       Ha duas maneiras de resolver isso:
-   *         1° - Voce pode fazer um merge da master em sua branch, no caso a que voce esta trabalhando
-   *              Porem os commits feito por voce e pelo outro programador fica todos baguncado.
+   *    Ha duas maneiras de resolver isso:
+   *    1° - Voce pode fazer um merge da master em sua branch, no caso a que voce esta trabalhando
+   *         Porem os commits feito por voce e pelo outro programador fica todos baguncado.
    *         
-   *         2° - Fazendo o git rebase,que vai jogar seu codigo em cima do ultimo merge feito na master 
-   *              entao seu commits vai ficar separado do programador anterior,vai fica no topo dos logs. 
+   *    2° - Fazendo o git rebase,que vai jogar seu codigo em cima do ultimo merge feito na master 
+   *         entao seu commits vai ficar separado do programador anterior,vai fica no topo dos logs. 
    */
 }
