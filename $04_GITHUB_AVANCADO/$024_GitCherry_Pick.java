a82e2fede1d3036570be2e570513a4098dde291a
Fabiano <fasnaweb2020@gmail.com>
2022-03-28 20:40:02 -0300

    entendendo Cherry-pick

---
+++ $04_GITHUB_AVANCADO/$024_GitCherry_Pick.java
@@ -0,0 +1,8 @@
+package $04_GITHUB_AVANCADO;
+
+public class $024_GitCherry_Pick {
+	/*
+	 * Comando utilizado para mergear apenas os commits que voce deseja dentro da master
+	 * sendo assim utilizado apenas os commits desejados.
+	 */
+}
