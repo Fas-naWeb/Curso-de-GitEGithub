760c8f3258ffcdb2994aa8ed07c616c663989937
Fabiano <fasnaweb2020@gmail.com>
2022-03-29 18:44:55 -0300

    Recomecendo estudos 29/03/2022

--- $04_GITHUB_AVANCADO/$024_GitCherry_Pick.java
+++ $04_GITHUB_AVANCADO/$024_GitCherry_Pick.java
@@ -4,5 +4,8 @@ public class $024_GitCherry_Pick {
 	/*
 	 * Comando utilizado para mergear apenas os commits que voce deseja dentro da master
 	 * sendo assim utilizado apenas os commits desejados.
+	 * 
+	 * Primeiro voce deve ir para branch que voce quer mergear os log desejados.Depois digite
+	 * git cherry-pick + nome da branch + 3746a2c7ae31703e85247ef0e20170929354fff4
 	 */
 }
---
+++ $04_GITHUB_AVANCADO/$025_GitReset.java
@@ -0,0 +1,10 @@
+package $04_GITHUB_AVANCADO;
+
+public class $025_GitReset {
+	/*
+	 *  E um comando que reverte mudancas na sua branch.
+	 *  Quando voce faz um reset voce estara excluindo commits que não estaram fazendo parte
+	 *  da sua branch.
+	 *  voce pode escolher um rache,n qual sera o cabeca dos seus logs.
+	 */
+}
