1d9664698e60b9bf62d2fa5412437d930bf30a25
Fabiano <fasnaweb2020@gmail.com>
2022-03-29 19:03:43 -0300

    Organizei os commits

---
+++ $04_GITHUB_AVANCADO/$025_GitReset.java
@@ -0,0 +1,15 @@
+package $04_GITHUB_AVANCADO;
+
+public class $025_GitReset {
+	/*
+	 *  GIT RESET 
+	 *   E um comando que exclui todos comites acima dele deixando o seu commit escolhido no topo dos 
+	 *   outros commites.
+	 *   
+	 *   Entao a ideia e git reset + o rech escolhido.Os rechs acima do que voce escolheu sao deletados 
+	 *   porem nao excluidos.Voltam rapa ser adicionada formando um novo commit.
+	 *   
+	 *   git reset 421ee840c923d5296962d7c1bcdccdc2b94148b9
+
+	 */
+}
