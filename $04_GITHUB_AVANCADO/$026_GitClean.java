1c4de99c53e50cc86cec749a23af164ae007a119
Fabiano <fasnaweb2020@gmail.com>
2022-03-29 20:57:19 -0300

    Entendendo git clean

---
+++ $04_GITHUB_AVANCADO/$026_GitClean.java
@@ -0,0 +1,12 @@
+package $04_GITHUB_AVANCADO;
+
+public class $026_GitClean {
+	/*
+	 * GIT CLEAN
+	 *   Comando utilizado para limpar as alteracoes feitas nos arquivos antes de os mesmos
+	 *   ter cido adicionado a area de stege.Antes do git add.Ou depois de ter utilizado o git reset.
+	 *   
+	 *   ex: git clean -f    Exclui todos os documentos.
+	 *   
+	 */
+}
