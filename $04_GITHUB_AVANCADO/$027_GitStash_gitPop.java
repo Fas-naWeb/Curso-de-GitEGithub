c1620abd544490f464c533d9a60f34416e30f65c
Fabiano <fasnaweb2020@gmail.com>
2022-04-03 15:40:59 -0300

    Entendi como funciona

--- $04_GITHUB_AVANCADO/$027_GitStash_gitPop.java
+++ $04_GITHUB_AVANCADO/$027_GitStash_gitPop.java
@@ -13,6 +13,12 @@ public class $027_GitStash_gitPop {
 	 *  
 	 *  com git stash voce tem que adicionar ( git add .),como se fosse fazer um commit,
 	 *  porem voce esta adicionando os pontos de codigo numa pilha,sem comitalos. 
+	 *  
+	 *  SEQUENCIA DOS COMANDOS
+	 *   git add -> Primeiro adiciona o arquivo
+	 *   git stash -> Salva o arquivo numa lista para voce recuperalos depois.
+	 *   git stash list -> Mostra uma lista dos stash salvos
+	 *   git stash pop -> Retorna os arquivos para voce comita-los ou removelos
 	 */
 }
 
