2aa4252ae8c79c25dd828f440248995e70a665e0
Fabiano <fasnaweb2020@gmail.com>
2022-04-03 20:13:02 -0300

    Analisando Pull Request

---
+++ $05_GITHUB_PARTE2/$028_Analisando_BranchECommits.java
@@ -0,0 +1,13 @@
+package $05_GITHUB_PARTE2;
+
+public class $028_Analisando_BranchECommits {
+  /*
+   * Clicando em branchs voce vai ter varios recursos para analisar
+   * 
+   * OverView ->  (Visao Geral)
+   * yours    ->  (Suas Branches)
+   * Active   ->  (Branches Ativas)  
+   * Stale    ->  (Branches Absoletas)	
+   * All Branches (Todas Branches)
+   */
+}
---
+++ $05_GITHUB_PARTE2/$029_Sobre_PullRequest.java
@@ -0,0 +1,7 @@
+package $05_GITHUB_PARTE2;
+
+public class $029_Sobre_PullRequest {
+	/*
+	 * Pull Request e o ato de mergea sua branch em outra branch.
+	 */
+}
