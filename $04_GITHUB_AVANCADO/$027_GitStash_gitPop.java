71d554671d1cf0a4bfa9887affb97be51c1a7a41
Fabiano <fasnaweb2020@gmail.com>
2022-04-03 13:49:39 -0300

    Sobre git stash

--- $04_GITHUB_AVANCADO/$027_GitStash_gitPop.java
+++ $04_GITHUB_AVANCADO/$027_GitStash_gitPop.java
@@ -2,6 +2,14 @@ package $04_GITHUB_AVANCADO;
 
 public class $027_GitStash_gitPop {
 	/*
-	 * 
+	 * GIT STASH
+	 *  Imagine que voce esta codificando,mais não sabe se esse codigo vai ser seu 
+	 *  codigo final,Git stash e um comando que voce pode ficar guardando trechos de codigo 
+	 *  onde voce pode adiciona - los depois.
+	 *  Ex: Sabe aqueles momentos em que voce fica copiando e colando varios tre-
+	 *  chos guardando para utilizar depois,então git stash faz uma pilha desses codigos 
+	 *  ai voce resolve depois quais serao uteis para voce utilizar definitivaveme.Ou seja
+	 *  so vai valer mesmo depois que voce commita-los definitivamente.
 	 */
 }
+
