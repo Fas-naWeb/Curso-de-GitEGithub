6f19970bc6563979dbf941b035261821903663eb
Fabiano <fasnaweb2020@gmail.com>
2022-04-04 22:35:56 -0300

    Secao sobre git terminada,agora vamos para os bonus

---
+++ $05_GITHUB_PARTE2/$030_AdicionandoRevisoresParaPR.java
@@ -0,0 +1,17 @@
+package $05_GITHUB_PARTE2;
+
+public class $030_AdicionandoRevisoresParaPR {
+	/*
+	 * Voce pode pedir para alguem revisar seu codigo.
+	 * Clicando no seu Pull Request voce vai ter algumas abas no seu painel direito
+	 * 
+	 * COMO - Reviewers     -> Insere alguem para revisar seu pull request
+	 *      - Assignees     -> Sao pessoas que estao notificadas do seu pull request
+	 *      - Labels        -> Voce pode passar sobre o que e esse pull request
+	 *      - Projects      -> E uma opcao de criar projetos
+	 *      - Milestones    -> E um alvo,vomos supor que voce tem uma finalidade cada tres meses voce pode esta marcando aqui
+	 *      - Development   -> 
+	 *      - Notifications ->
+	 */
+	
+}
---
+++ $05_GITHUB_PARTE2/$031_ComoRevisarUmPullRequest.java
@@ -0,0 +1,18 @@
+package $05_GITHUB_PARTE2;
+
+public class $031_ComoRevisarUmPullRequest {
+	/*
+	 * Na pagina do pull request,voce vai ate adicionar um review
+	 * No botao reviewchanges,voce pode esta
+	 * - adicionando comentarios -
+	 * - comentando              -
+	 * - aprovando               -
+	 * - request changes -> Pedindo para arrumar o codigo -
+	 * 
+	 *  voce pode clicar no + e fazer um comentario sobre o codigo que voce que a mudanca
+	 *  No botao de adicionar review verdinho vai aparecer o numero de pontos que voce pediu 
+	 *  para arrumar.
+	 *  
+	 *  Depois voce vai selecionar request changes, e depois clicar em submit.
+	 */
+}
