5ebdf008c10690ddb2bc2ebf651eab577f18424a
fabiano alves <fasnaweb2020@gmail.com>
2022-04-06 21:51:22 -0300

    reiniciando as aulas

--- $07_EXTRA_TREINOEDICAS/$036_CriandoArquivoAlias.java
+++ $07_EXTRA_TREINOEDICAS/$036_CriandoArquivoAlias.java
@@ -4,5 +4,7 @@ public class $036_CriandoArquivoAlias {
 	/*
 	 * ARQUIVO ALIAS
 	 *  Serve para voce guardar configuracoes pessoais para seu terminal
+	 *  
+	 *  Iniciando a aula novamente rrrrkkkkkk....
 	 */
 }
