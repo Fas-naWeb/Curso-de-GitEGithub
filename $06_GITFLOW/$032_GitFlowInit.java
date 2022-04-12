5d0e674a9ef7840723d920f29beefc423be060ae
fabiano alves <fasnaweb2020@gmail.com>
2022-04-12 20:27:03 -0300

    Aprendendo sobre git flow init

--- $06_GITFLOW/$032_GitFlowInit.java
+++ $06_GITFLOW/$032_GitFlowInit.java
@@ -12,6 +12,17 @@ public class $032_GitFlowInit {
 	 *  	
 	 *  
 	 *  	Branch name for production releases: [main] -> Producao final,e a que vai receber o que vai ser final.
+	 *      Which branch should be used for integration of the "next release"? -> E a branch que voce vai produzir suas features
+	 *      geralmente chamada de develop
+	 *      
+	 *      feature prefix? [] -> E a  forma de como voce vai identificar os prefixos da sua branch
+	 *      Bugfix branches? [bugfix/] -> E o nome do prefixo que voce da as branchs que vao tratar de gugs do seu sistema
+	 *      Release branches? [release/]  -> E as branchs que sao candidatas a i para producao
+	 *      
+
+
+
+
 
 	 */
 }
