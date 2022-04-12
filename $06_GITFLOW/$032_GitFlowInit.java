b94b7e523f60188f07e02ee17be5c2ab70aa6256
fabiano alves <fasnaweb2020@gmail.com>
2022-04-11 22:17:22 -0300

    Aprendendo sobre git flow init

--- $06_GITFLOW/$032_GitFlowInit.java
+++ $06_GITFLOW/$032_GitFlowInit.java
@@ -2,6 +2,13 @@ package $06_GITFLOW;
 
 public class $032_GitFlowInit {
 	/*
-	 * 
+	 * GIT FLOW 
+	 *  E uma extencao de bibliotecas do git,que otimiza os comandos em um unico comando.
+	 *  
+	 *  GIT FLOW INIT
+	 *  E o comando que inicializa os comandos do git flow no repositorio indicado.
+	 *  Entao voce não pode usar os comandos do git flow sem antes inicializa-los.
+	 *  	Passo 01 -  git flow init
+	 *  	
 	 */
 }
