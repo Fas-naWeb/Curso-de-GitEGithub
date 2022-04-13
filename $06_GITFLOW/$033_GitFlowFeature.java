adb92753989b8c50960fa3055f89d159eab27de0
fabiano alves <fasnaweb2020@gmail.com>
2022-04-12 21:52:46 -0300

    Aprendendo sobre git flow feature e git flow finish

--- $06_GITFLOW/$033_GitFlowFeature.java
+++ $06_GITFLOW/$033_GitFlowFeature.java
@@ -2,6 +2,24 @@ package $06_GITFLOW;
 
 public class $033_GitFlowFeature {
 	/*
-	 * 
+	 * SERVE PARA TRABALHAR COM FEATURE BRANCH
+	 *   
+	 * GIT FLOW FEATURE START + NOMEDA BRANCH
+	 *    Substitui tres comandos sao eles:
+	 *       - git branch -b baseado na branch develop -
+	 *       - automaticamente nomeia  com feature/NOMEBRANCH -
+	 *       - Passa a usar a nova branch -
+	 *   
+	 *   
+	 * GIT FLOW FEATURE FINISH + NOME DA BRANCH
+	 *    merge feature branch + NOME DA BRANCH DENTRO DE DEVELOP 
+	 *    git checkout develop 
+	 *    git branch + NOME DA BRANCH
+	 *    Deleta a feature branch 
+	 *    git branch -D + nome da branch
+	 *    muda para develop 
+	 *    git checkout develop
 	 */
+	
+	// Tudo terminado .....
 }
