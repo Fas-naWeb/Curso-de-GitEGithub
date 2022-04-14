c7584aa84d64c41c65a7dc2c4f8e947ac8b35a3f
fabiano alves <fasnaweb2020@gmail.com>
2022-04-13 21:56:10 -0300

    Aprendendo sobre git flow start,finish e publish

--- $06_GITFLOW/$033_GitFlowFeature.java
+++ $06_GITFLOW/$033_GitFlowFeature.java
@@ -28,5 +28,5 @@ public class $033_GitFlowFeature {
 	 *     git push --set-upstream origin feature + NOME DA BRANCH 
 	 */
 	
-	// Tudo terminado .....
+	 // Fazendo um possivel teste da branch nova
 }
