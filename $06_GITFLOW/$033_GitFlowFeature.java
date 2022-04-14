2b8f167edd4a1bae30c9d14d4386252a9d232b3f
fabiano alves <fasnaweb2020@gmail.com>
2022-04-13 22:50:17 -0300

    Aprendendo sobre git flow feature

--- $06_GITFLOW/$033_GitFlowFeature.java
+++ $06_GITFLOW/$033_GitFlowFeature.java
@@ -5,27 +5,29 @@ public class $033_GitFlowFeature {
 	 * SERVE PARA TRABALHAR COM FEATURE BRANCH
 	 *   
 	 * GIT FLOW FEATURE START + NOMEDA BRANCH
-	 *    Substitui tres comandos sao eles:
+	 *         O QUE ELE FAZ,SUBSTITUI TRES COMANDOS
 	 *       - git branch -b baseado na branch develop -
 	 *       - automaticamente nomeia  com feature/NOMEBRANCH -
 	 *       - Passa a usar a nova branch -
 	 *   
 	 *   
 	 * GIT FLOW FEATURE FINISH + NOME DA BRANCH
-	 *   -  merge feature branch + NOME DA BRANCH DENTRO DE DEVELOP 
-	 *       git checkout develop 
 	 *    
-	 *   - git branch + NOME DA BRANCH
-	 *       deleta a feature branch 
-	 *       git branch -D + nome da branch
-	 *       
-	 *   - muda para develop 
-	 *       git checkout develop
+	 *   O QUE ELE FAZ,SUBSTITUI TRES COMANDOS
+	 *   
+	 *   -  git merge feature + NOME DA BRANCH.(MERGEIA DENTRO DE DEVELOP)
+	 *    
+	 *   - git branch  -D + NOME DA BRANCH
+	 *       deleta a feature branch que acabou de ser mergeada
+	 *   
+	 *   - git checkout develop 
+	 *     muda para develop 
+	 *    
 	 *    
 	 *    
 	 * GIT FLOW FEATURE PUBLISH + NOME DA BRANCH
-	 *   - cria uma branch remota
-	 *     git push --set-upstream origin feature + NOME DA BRANCH 
+	 *   - cria uma branch remota e a publica no github
+	 *   substitui esse comando -> git push --set-upstream origin feature + NOME DA BRANCH 
 	 */
 	
 	
