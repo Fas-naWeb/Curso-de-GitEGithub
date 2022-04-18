9773e16fe852cb96d3bbe70d75aede73aa7839c4
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 19:51:00 -0300

    Como criar um repositorio no github

--- $03_GITHUB_PARTE1/$012_CriandoUmRepositorioGitHub.java
+++ $03_GITHUB_PARTE1/$012_CriandoUmRepositorioGitHub.java
@@ -5,7 +5,7 @@ public class $012_CriandoUmRepositorioGitHub {
 	 *  CRIANDO UM REPOSITORIO NO GITHUB
 	 *    1° - Primeiro e necessario acessar o site do github e fazer o login
 	 *    2° - Na sua perfil inicial clique no sinal de + oulado do simbolo de um sininho na parte superior dereita.
-	 *    3° - Clique em novo repositorio
+	 *    3° - Clique em novo repositorio.
 	 *    4° - Na pagina que abrir insira um nome e uma descricao nos campos correspondentes,selecione public se for 
 	 *         um repositorio publico ou selecione privado se voce for criar um repositorio privado.
 	 *    5° - Selecione um readme e um gitignore se voce achar necessario
