499448275872f61ce427b39a826d59d80a56d122
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 19:44:50 -0300

    O que e git flow

--- $01_INTRODUCAO/$008_OqueEgitFlow.java
+++ $01_INTRODUCAO/$008_OqueEgitFlow.java
@@ -5,6 +5,6 @@ public class $008_OqueEgitFlow {
 	 * GIT FLOW
 	 * 	E um pacote de comandos que reuni varios comandos em um comando so.
 	 *  voce pode utilizar um comando que tem varias funcoes e que voce resolve 
-	 *  com apenas um linha de comando
+	 *  com apenas um linha de comando.
 	 */
 }
