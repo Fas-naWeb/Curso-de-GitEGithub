840011ae9346b9341d02f4928959930cfe19c88b
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 20:52:14 -0300

    Entendendo git flow hotfix

--- $06_GITFLOW/$035_GitFlowHotFix.java
+++ $06_GITFLOW/$035_GitFlowHotFix.java
@@ -2,6 +2,25 @@ package $06_GITFLOW;
 
 public class $035_GitFlowHotFix {
 	/*
-	 * 
+	 * GIT FLOW HOTFIX START + O NOME DA BRANCH
+	 *  1° Cria uma branch baseada na master
+	 *     git checkout -b
+	 *     
+	 *  2° Automaticamente nomeia 
+	 *     hotfix nomedabranch
+	 *     
+	 *  3° Passa usar a nova branch
+	 *     git checkout + nome da branch
+	 *     
+	 *     
+	 * GIT FLOW HOTFIX FINISH + NOME DA BRANCH
+	 *  1° Troca de branch,vai para master
+	 *     git checkout master
+	 *     
+	 *  2° Mergea a branch dentro da master
+	 *     git merge + nome da branch
+	 *     
+	 *  3° nossa esse comando e bem grande....
+	 *   
 	 */
 }
