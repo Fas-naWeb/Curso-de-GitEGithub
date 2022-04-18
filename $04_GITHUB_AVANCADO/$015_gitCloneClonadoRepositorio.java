72605b3d874db6c85c8640e0e732e85bf34d380f
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 20:09:37 -0300

    como clonar um repositorio

--- $04_GITHUB_AVANCADO/$015_gitCloneClonadoRepositorio.java
+++ $04_GITHUB_AVANCADO/$015_gitCloneClonadoRepositorio.java
@@ -5,6 +5,6 @@ public class $015_gitCloneClonadoRepositorio {
 	 * 	GIT CLONE
 	 *    Voce vai ate o repositorio no qual voce quer clonar(e baixar esse repositorio para uma pasta no seu pc)
 	 *    e copiar a url desse repositorio.
-	 *    commando e: git clone + a url do seu repositorio
+	 *    commando e: git clone + a url do seu repositorio..
 	 */
 }
