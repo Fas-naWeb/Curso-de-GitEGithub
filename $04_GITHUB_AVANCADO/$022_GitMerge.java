666f8131011699690d1d76cd37c028405aa528e1
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 20:16:32 -0300

    Entendendo como funciona git merge

--- $04_GITHUB_AVANCADO/$022_GitMerge.java
+++ $04_GITHUB_AVANCADO/$022_GitMerge.java
@@ -3,7 +3,7 @@ package $04_GITHUB_AVANCADO;
 public class $022_GitMerge {
 	/*
 	 * 	GIT MERGE
-	 * 	  Esse comando mescla as branchs a branch principal.
+	 * 	  Esse comando mescla as branchs a branch principal..
 	 *    git merge master
 	 *    
 	 *    Sempre que voce quizer mergea uma branch a branch principal,primeiro voce entra da branch principal 
