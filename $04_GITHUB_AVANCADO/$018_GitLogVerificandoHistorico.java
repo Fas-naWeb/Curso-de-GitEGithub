b15518e6ff9e7db928f03c1a02369133048ef056
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 20:12:12 -0300

    Verificando o historico de commits com git log

--- $04_GITHUB_AVANCADO/$018_GitLogVerificandoHistorico.java
+++ $04_GITHUB_AVANCADO/$018_GitLogVerificandoHistorico.java
@@ -6,7 +6,7 @@ public class $018_GitLogVerificandoHistorico {
 	 *   E uma funcao do git que te ajuda a verificar o que ja foi feito,ele te da um historico dos seus commits.
 	 *   git log
 	 *   
-	 *   Os primeiros commits sao os atuais.
+	 *   Os primeiros commits sao os atuais..
 	 *   
 	 */
 }
