b6d0af85a3901ec7baa335d6db79d7686f3356a3
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 19:48:18 -0300

    Como criar uma conta no gitHub

--- $02_GITBASICO/$010_CriandoContaGithub.java
+++ $02_GITBASICO/$010_CriandoContaGithub.java
@@ -3,6 +3,6 @@ package $02_GITBASICO;
 public class $010_CriandoContaGithub {
 	/*
 	 * Primeiro voce entra no site do github e cria uma conta,que pode ser uma conta gratuita ou paga
-	 * Se voce estiver trabalhando em uma empresa  e logico que voce vai ciar uma conta privada. 
+	 * Se voce estiver trabalhando em uma empresa  e logico que voce vai ciar uma conta privada.. 
 	 */
 }
