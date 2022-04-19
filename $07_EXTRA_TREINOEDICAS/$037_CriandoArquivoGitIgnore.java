0eec5f579c70a5ee5300c2a5c7f005670385f94f
fabiano alves <fasnaweb2020@gmail.com>
2022-04-19 20:04:40 -0300

    Entendendo como funciona o gitignore

---
+++ $07_EXTRA_TREINOEDICAS/$037_CriandoArquivoGitIgnore.java
@@ -0,0 +1,12 @@
+package $07_EXTRA_TREINOEDICAS;
+
+public class $037_CriandoArquivoGitIgnore {
+	/**
+	 * GIT IGNORE -> Ele serve para ignorar arquivos, então se voce colocar 
+	 * um arquivo dentro do gitignore,quando voce fizer um push ele nem vai 
+	 * verificar ele simplismente ignora esse arquivo.
+	 * 
+	 * Como exemplo eu vou criar um arquivo com nome teste.txt e vou lanca-lo 
+	 * no gitHub.Para verificarmos se realmente ele esta sendo ignorado.
+	 */
+}
