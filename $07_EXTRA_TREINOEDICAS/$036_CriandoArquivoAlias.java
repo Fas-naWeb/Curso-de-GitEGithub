0a50334a198710819a2796cccfa6fc871b154d90
fabiano alves <fasnaweb2020@gmail.com>
2022-04-19 19:38:29 -0300

    Simplificando comandos com alias

--- $07_EXTRA_TREINOEDICAS/$036_CriandoArquivoAlias.java
+++ $07_EXTRA_TREINOEDICAS/$036_CriandoArquivoAlias.java
@@ -5,5 +5,9 @@ public class $036_CriandoArquivoAlias {
 	 * ARQUIVO ALIAS
 	 *  e um atalho que voce cria para um comando maior,isso faz com que 
 	 *  voce guarde configuracoes pessoais para seu terminal.
+	 *  
+	 *  Uma das formas de criar o arquivo alias:
+	 *  git config --global alias.gt status
+	 *  git config --global alias.gp push
 	 */
 }
