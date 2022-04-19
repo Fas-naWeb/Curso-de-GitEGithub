d415f7e28e1750cb917f3ed7f622791e41653784
fabiano alves <fasnaweb2020@gmail.com>
2022-04-18 22:28:57 -0300

    Simplificando comandos com alias

--- $07_EXTRA_TREINOEDICAS/$036_CriandoArquivoAlias.java
+++ $07_EXTRA_TREINOEDICAS/$036_CriandoArquivoAlias.java
@@ -5,11 +5,5 @@ public class $036_CriandoArquivoAlias {
 	 * ARQUIVO ALIAS
 	 *  e um atalho que voce cria para um comando maior,isso faz com que 
 	 *  voce guarde configuracoes pessoais para seu terminal.
-	 *  
-	 * No meu caso criei a pasta bashrc dentro da pasta oculta .git
-	 * 
-	 * o comando para criar e abrir seu arquivo pode ser criado assim:
-	 *   vi + o nome que voce dara ao arquivo.
-	 *   Ex: vi .bashrc
 	 */
 }
