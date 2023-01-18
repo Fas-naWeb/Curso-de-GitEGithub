a95e300f697c93be70205b90ce9cfc052c776d5e
fabiano alves <fasnaweb2020@gmail.com>
2023-01-18 19:01:55 -0300

    Instalação do git no Linux

--- CURSO_DE_GIT/$01_INTRODUCAO/$002_InstalacaoDoGit.java
+++ CURSO_DE_GIT/$01_INTRODUCAO/$002_InstalacaoDoGit.java
@@ -5,6 +5,6 @@ public class $002_InstalacaoDoGit {
 	 * INSTALANDO O GIT
 	 *    sudo apt install git -> Comando de instalacao do git.
 	 *       
-	 *    git --version -> Mostra a versao do git instalado
+	 *    git --version -> Mostra a versao do git instalado.
 	 */	
 }
