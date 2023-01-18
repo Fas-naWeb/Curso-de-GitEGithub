7d0826aa1af8d25cdcab47dc7bb994b66dceaaa1
fabiano alves <fasnaweb2020@gmail.com>
2023-01-18 18:55:14 -0300

    O que é git hub

--- CURSO_DE_GIT/$01_INTRODUCAO/$007_OqueEgitHub.java
+++ CURSO_DE_GIT/$01_INTRODUCAO/$007_OqueEgitHub.java
@@ -6,7 +6,7 @@ public class $007_OqueEgitHub {
 	 * 	Github é uma plataforma online que voce pode esta visualizando o versionamento do seu projeto.
 	 *  Atraves dessa plataforma voce pode criar projetos tantos publicos como privado,alem de fazer alteracoes
 	 *  como criar branchs dar comandos como commits e merges.Mais o que e importante e que ele e uma interface 
-	 *  que voce pode esta visualiando seus projetos..
+	 *  que voce pode esta visualiando seus projetos...
 	 * 
 	 */
 }
