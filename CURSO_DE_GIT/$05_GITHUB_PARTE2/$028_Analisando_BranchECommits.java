5d844eb101ebb59f39c6a6878e1a9b8bfd35208e
fabiano alves <fasnaweb2020@gmail.com>
2023-01-19 00:39:44 -0300

    Analisando branchs

--- CURSO_DE_GIT/$05_GITHUB_PARTE2/$028_Analisando_BranchECommits.java
+++ CURSO_DE_GIT/$05_GITHUB_PARTE2/$028_Analisando_BranchECommits.java
@@ -2,7 +2,7 @@ package $05_GITHUB_PARTE2;
 
 public class $028_Analisando_BranchECommits {
   /*
-   * Clicando em branchs voce vai ter varios recursos para analisar.
+   * Clicando em branchs voce vai ter varios recursos para analisar..
    * 
    * OverView ->  (Visao Geral)
    * yours    ->  (Suas Branches)
