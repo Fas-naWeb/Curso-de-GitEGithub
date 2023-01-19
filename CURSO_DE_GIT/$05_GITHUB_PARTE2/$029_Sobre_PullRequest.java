025e7c5d4be4ea042b2258999ac2eef24cd7260c
fabiano alves <fasnaweb2020@gmail.com>
2023-01-19 00:40:40 -0300

    Sobre PullRequest

--- CURSO_DE_GIT/$05_GITHUB_PARTE2/$029_Sobre_PullRequest.java
+++ CURSO_DE_GIT/$05_GITHUB_PARTE2/$029_Sobre_PullRequest.java
@@ -2,6 +2,6 @@ package $05_GITHUB_PARTE2;
 
 public class $029_Sobre_PullRequest {
 	/*
-	 * Pull Request e o ato de mergea sua branch em outra branch..
+	 * Pull Request e o ato de mergea sua branch em outra branch...
 	 */
 }
