4f36f9ca9ade6cd0dc65431d872e7796402289ca
fabiano alves <fasnaweb2020@gmail.com>
2023-01-18 23:21:59 -0300

    Sobre o git Fetch

--- CURSO_DE_GIT/$04_GITHUB_AVANCADO/$017_GitFetchAtualizandoSuasBranchs.java
+++ CURSO_DE_GIT/$04_GITHUB_AVANCADO/$017_GitFetchAtualizandoSuasBranchs.java
@@ -6,7 +6,7 @@ public class $017_GitFetchAtualizandoSuasBranchs {
 	 *    O git fetch e um comando que te mostra se sua branch esta atualizada ou não,ele faz um comparativo 
 	 *    entre seu repositorio local com o repositorio do github,apos ele voce da um git status que mostra a 
 	 *    situacao da branch. O fetch, ele nao atualiza ele simplesmente tras a informacao de como esta  a relacao 
-	 *    entre os dois repositorios..
+	 *    entre os dois repositorios...
 	 *    
 	 *    Resumindo 
 	 *    git fetch
--- README.md
+++ README.md
@@ -103,6 +103,12 @@ INTRODUÇÃO
     <a href ="https://github.com/Fas-naWeb/Curso-de-GitEGithub---Concluido/blob/main/CURSO_DE_GIT/%2404_GITHUB_AVANCADO/%24016_gitPullBaixandoCommits.java">
       Sobre git pull
     </a>
+  </li>
+
+   <li>
+    <a href ="https://github.com/Fas-naWeb/Curso-de-GitEGithub---Concluido/blob/main/CURSO_DE_GIT/%2404_GITHUB_AVANCADO/%24017_GitFetchAtualizandoSuasBranchs.java">
+      Sobre git fetch
+    </a>
   </li> 
  </ol>
 
